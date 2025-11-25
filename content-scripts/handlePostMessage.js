c1732df10e7a913c459ee56e81809824ead94546
newtondotcom <asphalt9fr@gmail.com>
2025-11-26 00:34:13 +0100

    chore: add a `console.log` for os with no notification

--- content-scripts/handlePostMessage.js
+++ content-scripts/handlePostMessage.js
@@ -2,6 +2,7 @@ console.log("handlePostMessage Loaded");
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	if (sender.id === chrome.runtime.id) {
+		console.log("handlePostMessage message", message);
 		window.postMessage({
 			type: "EDPU_MESSAGE",
 			payload: message
